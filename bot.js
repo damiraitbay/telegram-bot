ddd6b9dddd28ccb2c8fa62c789c1bbbb58b212d6
damiraitbay <120569864+damiraitbay@users.noreply.github.com>
2024-03-13 14:36:43 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const mongoose = require('mongoose');
 
 // Строка подключения к базе данных MongoDB Atlas
-const dbURI = 'mongodb+srv://Damir1:damir270804@cluster0.ffeliay.mongodb.net/';
+const dbURI = 'mongodb+srv://Damir:damir270804@cluster0.ffeliay.mongodb.net/';
 
 // Подключение к базе данных
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
